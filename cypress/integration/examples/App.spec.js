79d53626d94023537750a96099e0a3e39320bb21
Leanna Hue <leanna.hue@gmail.com>
2020-03-10 20:49:48 -0500

    cypress passed test 2

--- cypress/integration/examples/App.spec.js
+++ cypress/integration/examples/App.spec.js
@@ -3,4 +3,10 @@ describe ('Test App', () => {
   it ('launches', () => {
     cy.visit ('/');
   });
+
+  it ('opens with App Bar title', () => {
+    cy.visit ('/');
+    cy.get('[data-cy=app-top-bar]').should('contain', 'Thrift Shift');
+  });
+
 });
--- src/App.js
+++ src/App.js
@@ -5,38 +5,65 @@ import TopAppBar from "./components/TopAppBar";
 import ItemForm from "./components/ItemForm";
 import ProductList from "./components/ProductList";
 import { updateUserState } from "./utils/FirebaseAuthUtils";
-import { getUserProductsInfo, getAllProductInfo, addRole, getRole, addUserInfo } from "./utils/FirebaseDbUtils"
+import {
+  getUserProductsInfo,
+  getAllProductInfo,
+  addRole,
+  getRole,
+  addUserInfo
+} from "./utils/FirebaseDbUtils";
 import "./App.css";
 import Listings from "./components/Listings/Listings";
-
 
 const App = () => {
   const [user, setUser] = useState(null);
   const [productIds, setProductIds] = useState(null);
   const [userRole, setUserRole] = useState(null);
   const [open, setOpen] = useState(false);
-  const [page, setPage] = React.useState('product')
+  const [page, setPage] = React.useState("product");
 
   const handleClose = () => {
-    setOpen(false)
+    setOpen(false);
   };
 
   const ChooseRole = ({ user }) => {
     return (
-      <Dialog
-        open={open}
-        onClose={handleClose}
-      >
-        <DialogTitle id="simple-dialog-title" style={{ textAlign: 'center' }}>Choose Your Role</DialogTitle>
-        <Button variant="contained" color="primary" style={{ width: '50%', marginLeft: '25%' }} onClick={() => { addRole(user.uid, "seller"); getRole(user.uid, setUserRole); handleClose() }}>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle id="simple-dialog-title" style={{ textAlign: "center" }}>
+          Choose Your Role
+        </DialogTitle>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ width: "50%", marginLeft: "25%" }}
+          onClick={() => {
+            addRole(user.uid, "seller");
+            getRole(user.uid, setUserRole);
+            handleClose();
+          }}
+        >
           Seller
-      </Button>
-        <Button variant="contained" color="primary" style={{ marginTop: '5px', marginBottom: '5px', width: '50%', marginLeft: '25%' }} onClick={() => { addRole(user.uid, "buyer"); getRole(user.uid, setUserRole); handleClose() }}>
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{
+            marginTop: "5px",
+            marginBottom: "5px",
+            width: "50%",
+            marginLeft: "25%"
+          }}
+          onClick={() => {
+            addRole(user.uid, "buyer");
+            getRole(user.uid, setUserRole);
+            handleClose();
+          }}
+        >
           Buyer
-      </Button>
+        </Button>
       </Dialog>
-    )
-  }
+    );
+  };
 
   // Change user state when the user successfully logged in
   useEffect(() => {
@@ -59,30 +86,32 @@ const App = () => {
   useEffect(() => {
     if (userRole) {
       if (userRole === "buyer") {
-        getAllProductInfo(setProductIds)
+        getAllProductInfo(setProductIds);
       }
       if (userRole === "seller") {
-        getUserProductsInfo(user.uid, setProductIds)
+        getUserProductsInfo(user.uid, setProductIds);
       }
     }
   }, [userRole]);
 
   return (
     <Container disableGutters>
-      <div style={{ height: '10px' }} />
+      <div style={{ height: "10px" }} />
       <ChooseRole user={user} />
-      <TopAppBar user={user} userRole={userRole} setPage={setPage} />
-      {
-        page === 'product' ?
-          <div>
-            <ItemForm userRole={userRole} />
-            <ProductList productIds={productIds} user={user} userRole={userRole} />
-          </div> : null
-      }
-      {
-        page === 'bid' ?
-          <Listings productIds={productIds} /> : null
-      }
+      <div data-cy="app-top-bar">
+        <TopAppBar user={user} userRole={userRole} setPage={setPage} />
+      </div>
+      {page === "product" ? (
+        <div>
+          <ItemForm userRole={userRole} />
+          <ProductList
+            productIds={productIds}
+            user={user}
+            userRole={userRole}
+          />
+        </div>
+      ) : null}
+      {page === "bid" ? <Listings productIds={productIds} /> : null}
     </Container>
   );
 };
